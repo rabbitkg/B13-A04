e764f93f33f5fa4a37f08fd2dd19e904be8fc779
rabbitkg <rabbitkgbd@gmail.com>
2026-02-22 01:26:21 +0600

    added java scritp funtion

--- scritp/script.js
+++ scritp/script.js
@@ -1,3 +1,9 @@
+let interviewList = [];
+let rejectedList = [];
+
+
+
+
 let total = document.getElementById("total");
 let interviewCount = document.getElementById("interview");
 let rejectedCount = document.getElementById("rejected");
@@ -5,10 +11,23 @@ let rejectedCount = document.getElementById("rejected");
 let jobsCount = document.getElementById("jobs");
 
 const allCardSection = document.getElementById("allCard");
-console.log(allCardSection.children.length);
+
+const mainContainer = document.querySelector('main');
+console.log(mainContainer);
+
+// const allFilterBtn = document.getElementById("all-filter-btn")
+// allFilterBtn.addEventListener("click", function(){
+//     alert("click from add even")
+// })
 
 function calculateCount(){
     total.innerText = allCardSection.children.length;
     jobsCount.innerText = allCardSection.children.length;
+    interviewCount.innerText = interviewList.length;
+    rejectedCount.innerText = rejectedList.length;
 }
 calculateCount();
+
+function toggleStyle(id){
+    console.log("click");
+}
