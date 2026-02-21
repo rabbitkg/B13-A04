1a87e982efbcbe1cb4b5444024205bb318294cf6
rabbitkg <rabbitkgbd@gmail.com>
2026-02-22 00:22:26 +0600

    third commit

--- scritp/script.js
+++ scritp/script.js
@@ -1,3 +1,14 @@
 let total = document.getElementById("total");
 let interviewCount = document.getElementById("interview");
 let rejectedCount = document.getElementById("rejected");
+
+let jobsCount = document.getElementById("jobs");
+
+const allCardSection = document.getElementById("allCard");
+console.log(allCardSection.children.length);
+
+function calculateCount(){
+    total.innerText = allCardSection.children.length;
+    jobsCount.innerText = allCardSection.children.length;
+}
+calculateCount();
